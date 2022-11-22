74ef9b0a2635611b6d9f81e7aa83d9a215f43fed
yaheda <yaheda.a@gmail.com>
2022-11-22 22:40:19 +0200

    lottery smart contract

--- 18Lottery/contracts/Lottery.sol
+++ 18Lottery/contracts/Lottery.sol
@@ -29,6 +29,35 @@ contract Lottery is Ownable {
     currentState = State.BETTING;
   }
 
+  function bet() external payable inState(State.BETTING) {
+    require(msg.value == requiredPrice, 'can only send exact amount');
+
+    players.push(payable(msg.sender));
+    if (players.length == requiredParticipants) {
+      // 1. pick a winner
+      // 2. send money
+      // 3. change state to idle
+      // 4. data cleanup
+
+      uint winner = _randomModulo(requiredParticipants);
+      payable(players[winner]).sendValue((requiredParticipants * requiredPrice) * (100 - houseFee) / 100);
+      currentState = State.IDLE;
+      delete players;
+    }
+  }
+
+  function cancel() external inState(State.BETTING) onlyOwner {
+    for(uint i = 0; i < players.length; i++) {
+      payable(players[i]).sendValue(requiredPrice);
+    }
+    delete players;
+    currentState = State.IDLE;
+  }
+
+  function _randomModulo(uint modulo) view internal returns(uint) {
+    return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % modulo;
+  }
+
   modifier inState(State _state) {
     require(currentState == _state, 'current state does not allow this');
     _;
