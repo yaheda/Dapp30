e1371ffdee7c1ad57f5af381553d4ac5c3c021ab
yaheda <yaheda.a@gmail.com>
2023-02-06 22:09:45 +0200

    mint frontend

--- 23CryptoKitties/app/src/Admin.js
+++ 23CryptoKitties/app/src/Admin.js
@@ -26,6 +26,11 @@ export default () => {
     <div>
       <div>
         <h2>Mint</h2>
+        <ContractForm 
+           drizzle={drizzle}
+           contract="CryptoKitty"
+            method="mint"
+        />
       </div>
     </div>
   );
