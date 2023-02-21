abc17516164068bc594c7c4561f27b904b861da8
yaheda <yaheda.a@gmail.com>
2023-02-21 22:52:03 +0200

    trade

--- 25Ebay/contracts/Ebay.sol
+++ 25Ebay/contracts/Ebay.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier
 pragma solidity 0.8.17;
 
+import "@openzeppelin/contracts/utils/Address.sol";
+
 contract Ebay {
   //1. Allow seller to create auction
   //2. Allow buyers to make offer for auction
   //3. Allow seller and buers to trade at end of an auction
+
+  using Address for address payable;
 
   struct Auction {
     uint id;
@@ -52,15 +56,37 @@ contract Ebay {
     nextAuctionId++;
   }
 
-  function createOffer(uint _auctionId) external payable {
+  function createOffer(uint _auctionId) external payable auctionExist(_auctionId) {
     Auction storage auction = auctions[_auctionId];
     Offer storage bestOffer = offers[auction.bestOfferId];
 
-    require(_auctionId > 0 && _auctionId < nextAuctionId, 'auction does not exist');
     require(block.timestamp < auction.endDate, 'auction has expired');
     require(msg.value >= auction.minPrice && msg.value > bestOffer.price, 'value must be more to auction min and bestoffer');
 
     auction.bestOfferId = nextOfferId;
+    auction.offerIds.push(nextOfferId);
     offers[nextOfferId] = Offer(nextOfferId, payable(msg.sender), _auctionId, msg.value);
+  }
+
+  function trade(uint _auctionId) external auctionExist(_auctionId) {
+    Auction storage auction = auctions[_auctionId];
+    Offer storage bestOffer = offers[auction.bestOfferId];
+
+    require(block.timestamp >= auction.endDate, 'auction has not expired');
+
+    for(uint i = 0; i < auction.offerIds.length; i++) {
+      uint offerId = auction.offerIds[i];
+      if (offerId != auction.bestOfferId) {
+        Offer storage offer = offers[offerId];
+        payable(offer.buyer).sendValue(offer.price);
+      }
+    }
+
+    payable(auction.seller).sendValue(bestOffer.price);
+  }
+
+  modifier auctionExist(uint _auctionId) {
+    require(_auctionId > 0 && _auctionId < nextAuctionId, 'auction does not exist');
+    _;
   }
 }
